e74787c16f9005a71bb72f3a7ea17cc176af68ea
Po Linn Chia <chiapolinn@gmail.com>
2015-02-06 16:24:20 -0700

    Basic app structure and constructors

--- main.js
+++ main.js
@@ -1,3 +1,136 @@
+var DrnkMxr = (function(){
+
+  /////////////////
+  // DRINK CLASS //
+  /////////////////
+
+  var Drink = (function(){
+
+    /**
+     * Drink class constructor
+     * @param {string} name           Name of drink
+     * @param {string} base           Drink base liquor
+     * @param {array} ingredients     Array of ingredients
+     * @param {string} instructions   Full-text instructions
+     * @param {number} votes          Votes
+     */
+    var Drink = function(name, base, ingredients, instructions, votes){
+      
+      this.name = name;
+      this.base = base;
+      this.ingredients = ingredients;
+      this.instructions = instructions;
+      this.votes = votes || 0;
+    };
+
+
+    Drink.prototype.toString = function(){
+
+      return this.name + " (" + this.base + ")";
+    };
+
+
+    /**
+     * Drink rate method for changing votes 
+     * @param  {number} delta         +1 or -1 vote 
+     */
+    Drink.prototype.rate = function(delta){
+      
+      this.votes += Number(delta);
+    };
+
+    return Drink;
+
+  })(); //end Drink
+
+
+  ///////////////////
+  // CABINET CLASS //
+  ///////////////////
+
+  var Cabinet = (function(){
+
+    /**
+     * Cabinet class constructor
+     */
+    var Cabinet = function(){      
+      this.drinks = [];
+      this.ingredients = [];
+
+    };
+
+    /**
+     * Add a drink to a cabinet
+     * @param {string} name           Name of drink
+     * @param {string} base           Drink base liquor
+     * @param {array} ingredients     Array of ingredients
+     * @param {string} instructions   Full-text instructions
+     * @param {number} votes          Votes
+     */
+    Cabinet.prototype.addDrink = function(name, base, ingredients, instructions, votes){
+
+      // Add drink to the cabinet
+      var newDrink = new Drink(name, base, ingredients, instructions, votes);
+      this.drinks.push(newDrink);
+
+      // Update the master list of ingredients
+      _.union(this.ingredients, ingredients);
+    };
+
+    /**
+     * Cabinet toString method
+     * @return {string}               List of drinks with their bases
+     */
+    Cabinet.prototype.toString = function(){
+      return  _.map(this.drinks, function(drink){ return drink.toString(); }).join("\n");
+    };
+
+    /**
+     * Load in a series of drinks from a properly-defined array of objects
+     * @param  {array} array         Array of drink objects
+     */
+    Cabinet.prototype.autoLoad = function(array){
+      var cabinet = this;
+      _.each(array, function(drinkObj){
+       return cabinet.addDrink(drinkObj.name, drinkObj.base, drinkObj.ingredients, drinkObj.instructions, drinkObj.votes);
+      });
+    };
+
+
+
+    return Cabinet;
+
+
+  })(); //end Cabinet
+
+
+
+
+
+
+
+
+
+
+  // Return all the constructors
+  var DrnkMxr = {
+    Drink   : Drink,
+    Cabinet : Cabinet
+  };
+
+  return DrnkMxr;
+
+
+})();
+
+var myCabinet = new DrnkMxr.Cabinet();
+
+myCabinet.autoLoad(drinksList);
+
+console.log(myCabinet.toString());
+
+
+
 $(document).on('ready', function() {
   
 });
